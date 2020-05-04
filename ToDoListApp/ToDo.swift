39a0b0aa63552c365569f6c7a4b8d7ab068b6938
brandigehrke <brandi.f.gehrke@gmail.com>
2020-05-03 19:26:53 -0500

    Initial Commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Brandi Freeman on 5/3/20.
+//  Copyright © 2020 Brandi Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
